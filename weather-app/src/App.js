8d03a19fa56c82f18aacd7821ce354925f9a2b75
Aliaksandra Skachkova <skach.alexandra@gmail.com>
2021-06-22 21:11:59 +0200

    feat: searchbar

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// const api = {
+//   key: "98b7465353d383f3d0f3bc4a284a48ae",
+
+//   base: "http://api.openweathermap.org/data/2.5",
+// };
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="content">
+        <div className="search">
+          <input type="text" className="search-bar" placeholder="Search" />
+        </div>
+      </section>
     </div>
   );
 }
